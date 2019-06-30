aed86be8917da3fa74f6caa3e841f458500696b2
duehng_glb <duheng_pro@duheng.com>
2019-06-30 20:31:33 +0800

    Initial commit

---
+++ src/main/java/cn/idhclub/CommunityManageApplication.java
@@ -0,0 +1,13 @@
+package cn.idhclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/idhclub/CommunityManageApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.idhclub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunityManageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
