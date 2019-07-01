d65fd4420006a38d40ea5d1e1f5c2510a2c22cdb
duehng_glb <duheng_pro@duheng.com>
2019-07-01 16:16:13 +0800

    新闻添加

---
+++ src/main/java/cn/community/contronller/NewsContronller.java
@@ -0,0 +1,24 @@
+package cn.community.contronller;
+
+import cn.community.c_interface.NewsService;
+import cn.community.pojo.News;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/news")
+public class NewsContronller {
+    @Autowired
+    NewsService newsService;
+
+    @RequestMapping("/all")
+    public String getAllNews(Map<String, Object> map) {
+        List<News> allNews = newsService.getAllNews();
+        map.put("all", allNews);
+        return "index";
+    }
+}
